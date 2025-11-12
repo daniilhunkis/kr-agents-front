77152e6810159201dd2a813ce9ab17f08f615cde
Юшко Даниил Серафимович <yshko2003icloud.com@MacBook-Air-Usko.local>
2025-11-12 23:31:03 +0300

    fix: API base path

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://api.krd-agents.ru/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://api.krd-agents.ru",
 });
 
 export const createObject = (data: FormData) =>
